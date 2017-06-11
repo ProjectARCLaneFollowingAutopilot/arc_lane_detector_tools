3e07f2ec2e9e63b3395a3caf219fb3acb2c0312f
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-12 01:57:22 +0200

    Functions extended

--- include/ransac_fitting/ransac_fitting.hpp
+++ include/ransac_fitting/ransac_fitting.hpp
@@ -5,22 +5,25 @@ Input shall be: Set of points, number of iterations, thresholds.
 Output shall be: Parameters of the fitted function.
 */
 
+#include <cmath>
 #include <cv.h>
 #include <iostream>
 #include <vector>
 #include "opencv2/highgui/highgui.hpp"
 
 using namespace cv;
+//cv::Point2f init_points[4];
+
 
 // This struct stores all required informations of a specific consensus set.
 struct Consensus
 {
   // Random points, used for calculating this consensus set.
-  vector<Vec2f> random_points;  //(3);
+  vector<cv::Point2f> random_points; //(3);
   // Coefficients (a, b, c) of the polynom passing through the three random points.
-  std::vector<float> polynom_coeff;   //(3); 
+  vector<float> polynom_coeff;   //(3); 
   // Consensus set.
-  vector<Vec2f> cons_set;
+  vector<cv::Point2f> cons_set;
   // Size of the consensus set.
   int size_cons_set;
 };
@@ -33,8 +36,8 @@ class Ransac
   Ransac();
   // Standard destructor.
   ~Ransac();
-  // Method to assign new data points.
-  void setRansacDataSet(vector<Vec2f> &data_set);
+  // Method to assign new data points and also sets x_min_dataset_ and x_max_dataset_;
+  void setRansacDataSet(vector<cv::Point2f> &data_set);
   // Method to set the parameters of the RANSAC Algorithm (threshholds, number of iterations,...).
   void setRansacParams(float max_inlier_distance, int max_num_of_iterations, int min_size_consensus);
   // Method which does the RANSAC algorithm and returns the found polynomial parameters -->Master function.
@@ -46,11 +49,11 @@ class Ransac
   // Method which choses a random points from the data set.
   Point2f getRandomPoint();
   // Method which takes exactly the required number of points fits the polynom (-->fully determined) and returns the coefficients.
-  std::vector<float> getCoeffDet(vector<Vec2f> det_points);  
+  vector<float> getCoeffDet(vector<cv::Point2f> det_points);  
   // Method which returns the absolute distance from a given polynomial.
-  float getDistancePointToPolynom(Point2f point, vector<Vec2f> polynom_coeff);
+  float getDistancePointToPolynom(Point2f point, vector<float> polynom_coeff);
   // Method, which takes more than the required number of points, fits the polynom using LSQ (-->over determined) and returns the parameters.
-  std::vector<float> getCoeffLSQ(vector<Vec2f> over_det_points); 
+  vector<float> getCoeffLSQ(vector<cv::Point2f> over_det_points); 
   // PRIVATE MEMBER VARIABLES.
   // The maximum allowed distance (define distance!) from the curve, such that point counted as inlier.
   float max_inlier_distance_; 
@@ -58,10 +61,14 @@ class Ransac
   int max_num_of_iterations_;
   // Size of the consensus set, to be counted as a valable consensus set.
   int min_size_consensus_;
-
+  // Minimal x-coordinate which is apparent in the data set (required for discrete shortest distance finder).
+  int x_min_dataset_;
+  // Maximal x-coordinate which is apparent in the data set (required for discrete shortest distance finder).
+  int x_max_dataset_;
 
   // Vector which stores the data set of 2D pixel points.
-  vector<Vec2f> data_set_;
+  vector<cv::Point2f> data_set_;
   // Vector of structs, storing all of the consensus set structs.
   vector<Consensus> all_cons_sets_;
+  Consensus largest_consensus_set_;
 };
--- src/ransac_fitting/ransac_fitting.cpp
+++ src/ransac_fitting/ransac_fitting.cpp
@@ -13,9 +13,28 @@ Ransac::~Ransac()
 
 }
 // Method to assign new data points.
-void Ransac::setRansacDataSet(vector<Vec2f> &data_set)
+void Ransac::setRansacDataSet(vector<cv::Point2f> &data_set)
 {
+	// Copy the dataset.
 	this->data_set_ = data_set;
+	int x_min_dataset = 10000;
+	int x_max_dataset = -1000;
+	// Find the minimal and maximal x-coordinates in the data set.
+	for(int i = 0; i<data_set.size(); i++)
+	{
+		if((this->data_set_)[i].x < x_min_dataset)
+		{
+			x_min_dataset = (this->data_set_)[i].x;
+		}
+		else if((this->data_set_)[i].x > x_max_dataset)
+		{
+			x_max_dataset = (this->data_set_)[i].x;
+
+		}
+	}
+	// Set the found values.
+	this->x_min_dataset_ = x_min_dataset;
+	this->x_max_dataset_ = x_max_dataset;
 }
 
 // Method to set the parameters of the RANSAC Algorithm (threshholds, number of iterations,...).
@@ -26,23 +45,46 @@ void Ransac::setRansacParams(float max_inlier_distance, int max_num_of_iteration
 	this->min_size_consensus_ = min_size_consensus;
 }
 
-// Method which does the RANSAC algorithm and returns the found polynomial parameters -->Master function.
+// NOCH NICHT FERTIG: Method which does the RANSAC algorithm and returns the found polynomial parameters -->Master function.
 vector<float> Ransac::getRansacCoeff()
 {
 	// Loop as many times as previously defined to generate the consensus sets.
 	for(int i = 0; i < this->max_num_of_iterations_; i++)
 	{
-		// Do a random sample to find three points from the data set.
+		// Create a Consensus struct to store the data of the current iteration.
+		Consensus consensus_set_temp;
 
+		// FEHLT NOCH: Do a random sample to find three points from the data set. 
 		// Fit a first guess using the randomly chosen points.
+		consensus_set_temp.polynom_coeff = Ransac::getCoeffDet(consensus_set_temp.random_points);  
 
 		// Iterate through the whole data set and find the inliers and create a new Consensus variable to store.
-
-		// Append the consensus set vector with the just found consensus set.
-
+		for(int j = 0; j<this->data_set_.size(); j++)
+		{
+			float distance = Ransac::getDistancePointToPolynom(this->data_set_[j], temp_coeff);
+			if(distance < this->max_inlier_distance_)
+			{
+				consensus_set_temp.cons_set.pushback(this->data_set_[j]);
+			}
+			consensus_set_temp.size_cons_set = consensus_set_temp.cons_set.size();
+		}
+		// Append the consensus set vector with the just found consensus set struct.
+		if(consensus_set_temp.size_cons_set > this->min_size_consensus_)
+		{
+			this->all_cons_sets_.pushback(consensus_set_temp);
+		}
 	}
 
 	// Find the largest consensus set.
+	int size_of_largest_cons = 0;
+	for(int j = 0; i<this->all_cons_sets_.size();j++)
+	{
+		if(this->all_cons_sets_.size_cons_set > size_of_largest_cons)
+		{
+			this->largest_consensus_set_ = all_cons_sets_[j];
+			size_of_largest_cons = this->all_cons_sets_.size_cons_set;
+		}
+	}
 
 	// Do least square to find the final coefficients.
 
@@ -51,3 +93,36 @@ vector<float> Ransac::getRansacCoeff()
 }
 
 // PRIVATE MEMBER METHODS.
+
+ // FEHLT NOCH: Method which takes exactly the required number of points fits the polynom (-->fully determined) and returns the coefficients.
+ vector<float> Ransac::getCoeffDet(vector<cv::Point2f> det_points)
+ {
+
+ }
+
+
+
+// Method which returns the absolute distance from a given polynomial. 
+// !!! Effizienter wäre es für einen random set an Punkten das Polynom nur einmal zu diskretisieren.
+float Ransac::getDistancePointToPolynom(Point2f point, vector<float> polynom_coeff)
+{
+	// Iterate discretely through the polynomial to find the nearest point to the curve.
+	float resolution = 0.1;
+	vector<cv::Point2f> discrete_polynomial;
+	float minimal_distance = 1000.0;
+	for(float x = this->x_min_dataset_; x<this->x_max_dataset_; x = x + resolution)
+	{
+		float y = polynom_coeff[0]*pow(x,2) + polynom_coeff[1]*x + polynom_coeff[2];
+		// Evaluate polynomial at a discrete point.
+		cv::Point2f temp_coord(x, y);
+		// Calculate absolute distance to given point.
+		float distance = sqrt(pow(temp_coord.x - point.x,2) + pow(temp_coord.y - point.y,2));
+		// Decide if smallest distance have been found.
+		if(distance<minimal_distance)
+		{
+			minimal_distance = distance;
+		}
+	}
+	// Return the distance to the nearest point.
+	return minimal_distance;
+}
