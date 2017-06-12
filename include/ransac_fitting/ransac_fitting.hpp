23d55781e9ddfbe7dab5da4d86a046989e35a54a
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-12 15:18:42 +0200

    LSQ on its way!

--- include/ransac_fitting/ransac_fitting.hpp
+++ include/ransac_fitting/ransac_fitting.hpp
@@ -6,8 +6,11 @@ Output shall be: Parameters of the fitted function.
 */
 
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include <cv.h>
 #include <iostream>
+#include <Eigen/Dense>
 #include <vector>
 #include "opencv2/highgui/highgui.hpp"
 
@@ -19,11 +22,11 @@ using namespace cv;
 struct Consensus
 {
   // Random points, used for calculating this consensus set.
-  vector<cv::Point2f> random_points; //(3);
+  vector<Point2f> random_points; 
   // Coefficients (a, b, c) of the polynom passing through the three random points.
   vector<float> polynom_coeff;   //(3); 
   // Consensus set.
-  vector<cv::Point2f> cons_set;
+  vector<Point2f> cons_set;
   // Size of the consensus set.
   int size_cons_set;
 };
@@ -32,28 +35,28 @@ class Ransac
 {
   public:
   // PUBLIC MEMBER METHODS.
-  // Standard constructor.
+  // DONE: Standard constructor.
   Ransac();
-  // Standard destructor.
+  // DONE: Standard destructor.
   ~Ransac();
-  // Method to assign new data points and also sets x_min_dataset_ and x_max_dataset_;
+  // DONE: Method to assign new data points and also sets x_min_dataset_ and x_max_dataset_;
   void setRansacDataSet(vector<cv::Point2f> &data_set);
-  // Method to set the parameters of the RANSAC Algorithm (threshholds, number of iterations,...).
+  // DONE: Method to set the parameters of the RANSAC Algorithm (threshholds, number of iterations,...).
   void setRansacParams(float max_inlier_distance, int max_num_of_iterations, int min_size_consensus);
-  // Method which does the RANSAC algorithm and returns the found polynomial parameters -->Master function.
+  // DONE: Method which does the RANSAC algorithm and returns the found polynomial parameters -->Master function.
   vector<float> getRansacCoeff();
   // PUBLIC MEMBER VARIABLES.
 
   private:
   // PRIVATE MEMBER METHODS.
-  // Method which choses a random points from the data set.
+  // DONE: Method which choses a random points from the data set.
   Point2f getRandomPoint();
-  // Method which takes exactly the required number of points fits the polynom (-->fully determined) and returns the coefficients.
-  vector<float> getCoeffDet(vector<cv::Point2f> det_points);  
-  // Method which returns the absolute distance from a given polynomial.
+  // DONE: Method which takes exactly the required number of points fits the polynom (-->fully determined) and returns the coefficients.
+  vector<float> getCoeffDet(vector<Point2f> det_points);  
+  // DONE: Method which returns the absolute distance from a given polynomial.
   float getDistancePointToPolynom(Point2f point, vector<float> polynom_coeff);
   // Method, which takes more than the required number of points, fits the polynom using LSQ (-->over determined) and returns the parameters.
-  vector<float> getCoeffLSQ(vector<cv::Point2f> over_det_points); 
+  vector<float> getCoeffLSQ(vector<Point2f> over_det_points); 
   // PRIVATE MEMBER VARIABLES.
   // The maximum allowed distance (define distance!) from the curve, such that point counted as inlier.
   float max_inlier_distance_; 
@@ -67,7 +70,7 @@ class Ransac
   int x_max_dataset_;
 
   // Vector which stores the data set of 2D pixel points.
-  vector<cv::Point2f> data_set_;
+  vector<Point2f> data_set_;
   // Vector of structs, storing all of the consensus set structs.
   vector<Consensus> all_cons_sets_;
   Consensus largest_consensus_set_;
--- src/ransac_fitting/ransac_fitting.cpp
+++ src/ransac_fitting/ransac_fitting.cpp
@@ -1,18 +1,18 @@
 #include "../../include/ransac_fitting/ransac_fitting.hpp"
 
 // PUBLIC MEMBER METHODS.
-// Standard constructor.
+// DONE: Standard constructor.
 Ransac::Ransac()
 {
 	std::cout<<"RANSAC Object created!"<<std::endl;
 }
-// Standard destructor.
+// DONE: Standard destructor.
 Ransac::~Ransac()
 {
 	std::cout<<"RANSAC Object destroyed!"<<std::endl;
 
 }
-// Method to assign new data points.
+// DONE: Method to assign new data points.
 void Ransac::setRansacDataSet(vector<cv::Point2f> &data_set)
 {
 	// Copy the dataset.
@@ -20,16 +20,15 @@ void Ransac::setRansacDataSet(vector<cv::Point2f> &data_set)
 	int x_min_dataset = 10000;
 	int x_max_dataset = -1000;
 	// Find the minimal and maximal x-coordinates in the data set.
-	for(int i = 0; i<data_set.size(); i++)
+	for(int i = 0; i < this->data_set_.size(); i++)
 	{
-		if((this->data_set_)[i].x < x_min_dataset)
+		if(this->data_set_[i].x < x_min_dataset)
 		{
-			x_min_dataset = (this->data_set_)[i].x;
+			x_min_dataset = this->data_set_[i].x;
 		}
-		else if((this->data_set_)[i].x > x_max_dataset)
+		else if(this->data_set_[i].x > x_max_dataset)
 		{
 			x_max_dataset = (this->data_set_)[i].x;
-
 		}
 	}
 	// Set the found values.
@@ -37,7 +36,7 @@ void Ransac::setRansacDataSet(vector<cv::Point2f> &data_set)
 	this->x_max_dataset_ = x_max_dataset;
 }
 
-// Method to set the parameters of the RANSAC Algorithm (threshholds, number of iterations,...).
+// DONE: Method to set the parameters of the RANSAC Algorithm (threshholds, number of iterations,...).
 void Ransac::setRansacParams(float max_inlier_distance, int max_num_of_iterations, int min_size_consensus)
 {
 	this->max_inlier_distance_ = max_inlier_distance;
@@ -45,7 +44,7 @@ void Ransac::setRansacParams(float max_inlier_distance, int max_num_of_iteration
 	this->min_size_consensus_ = min_size_consensus;
 }
 
-// NOCH NICHT FERTIG: Method which does the RANSAC algorithm and returns the found polynomial parameters -->Master function.
+// DONE: Method which does the RANSAC algorithm and returns the found polynomial parameters -->Master function.
 vector<float> Ransac::getRansacCoeff()
 {
 	// Loop as many times as previously defined to generate the consensus sets.
@@ -54,12 +53,17 @@ vector<float> Ransac::getRansacCoeff()
 		// Create a Consensus struct to store the data of the current iteration.
 		Consensus consensus_set_temp;
 
-		// FEHLT NOCH: Do a random sample to find three points from the data set. 
+		// Do a random sample to find three points from the data set. 
+		for(int k = 0; k<3; k++)
+		{
+			Point2f random_point = this->Ransac::getRandomPoint();
+			consensus_set_temp.random_points.push_back(random_point);
+		}
 		// Fit a first guess using the randomly chosen points.
-		consensus_set_temp.polynom_coeff = Ransac::getCoeffDet(consensus_set_temp.random_points);  
+		consensus_set_temp.polynom_coeff = this->Ransac::getCoeffDet(consensus_set_temp.random_points);  
 
 		// Iterate through the whole data set and find the inliers and create a new Consensus variable to store.
-		for(int j = 0; j<this->data_set_.size(); j++)
+		for(int j = 0; j < this->data_set_.size(); j++)
 		{
 			float distance = Ransac::getDistancePointToPolynom(this->data_set_[j], consensus_set_temp.polynom_coeff);
 			if(distance < this->max_inlier_distance_)
@@ -94,35 +98,74 @@ vector<float> Ransac::getRansacCoeff()
 
 // PRIVATE MEMBER METHODS.
 
- // FEHLT NOCH: Method which takes exactly the required number of points fits the polynom (-->fully determined) and returns the coefficients.
- vector<float> Ransac::getCoeffDet(vector<cv::Point2f> det_points)
- {
+// DONE: Method which choses a random points from the data set.
+Point2f Ransac::getRandomPoint()
+{
+	std::srand(std::time(0)); // use current time as seed for random generator
+	int random_index = std::rand() % this->data_set_.size();  
+	return this->data_set_[random_index];
+}
 
+
+ // DONE: Method which takes exactly the required number of points fits the polynom (-->fully determined) and returns the coefficients.
+ // !!!Eigentlich könnte diese Funktion auch mit getCoeffLSQ gemacht werden!!!
+ vector<float> Ransac::getCoeffDet(vector<Point2f> det_points)
+ {
+ 	Eigen::Matrix3f A;
+  	Eigen::Vector3f b;
+  	A << pow(det_points[0].x,2),det_points[0].x,1,  pow(det_points[1].x,2),det_points[1].x,1,  pow(det_points[2].x,2),det_points[2].x,1;
+  	b << det_points[0].y, det_points[1].y, det_points[2].y;
+  	std::cout << "Here is the matrix A:\n" << A << std::endl;
+	std::cout << "Here is the vector b:\n" << b << std::endl;
+  	Eigen::Vector3f coeff = A.colPivHouseholderQr().solve(b);
+  	vector<float> coeff_copy;
+  	coeff_copy.push_back(coeff[0]);
+  	coeff_copy.push_back(coeff[1]);
+  	coeff_copy.push_back(coeff[2]);
+  	return coeff_copy;
  }
 
 
 
-// Method which returns the absolute distance from a given polynomial. 
+// DONE: Method which returns the absolute distance from a given polynomial. 
 // !!! Effizienter wäre es für einen random set an Punkten das Polynom nur einmal zu diskretisieren.
 float Ransac::getDistancePointToPolynom(Point2f point, vector<float> polynom_coeff)
 {
 	// Iterate discretely through the polynomial to find the nearest point to the curve.
 	float resolution = 0.1;
-	vector<cv::Point2f> discrete_polynomial;
 	float minimal_distance = 1000.0;
-	for(float x = this->x_min_dataset_; x<this->x_max_dataset_; x = x + resolution)
+	for(float x = this->x_min_dataset_; x < this->x_max_dataset_; x = x + resolution)
 	{
+		// Evaluate polynomial at a discrete point --> y(x).
 		float y = polynom_coeff[0]*pow(x,2) + polynom_coeff[1]*x + polynom_coeff[2];
-		// Evaluate polynomial at a discrete point.
-		cv::Point2f temp_coord(x, y);
+		// Save point on polynomial.
+		Point2f temp_coord(x, y);
 		// Calculate absolute distance to given point.
 		float distance = sqrt(pow(temp_coord.x - point.x,2) + pow(temp_coord.y - point.y,2));
-		// Decide if smallest distance have been found.
-		if(distance<minimal_distance)
+		// Decide if a smaller distance has been found.
+		if(distance < minimal_distance)
 		{
 			minimal_distance = distance;
-		}
+		} 
 	}
 	// Return the distance to the nearest point.
 	return minimal_distance;
 }
+
+ // Method, which takes more than the required number of points, fits the polynom using LSQ (-->over determined) and returns the parameters.
+ vector<float> Ransac::getCoeffLSQ(vector<Point2f> over_det_points)
+ {
+ 	// Create matrix A.
+ 	Eigen::MatrixXf A(over_det_points.size(),3);
+ 	// Create matrix b.
+ 	Eigen::VectorXf b(over_det_points.size());
+ 	// Fill Matrix A and Vector b.
+ 	for(int i = 0; i<over_det_points.size(); i++)
+ 	{
+ 		float x = over_det_points[i].x;
+ 		float y = over_det_points[i].x;
+ 		// Assign.
+ 		b[i] = y;
+ 		
+ 	}
+ } 
