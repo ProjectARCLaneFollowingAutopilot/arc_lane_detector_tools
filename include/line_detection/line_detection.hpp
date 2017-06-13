ae2378a21216abe88ac6cb872c370bef1aa10637
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-13 13:33:58 +0200

    Structure for LD class added

--- include/line_detection/line_detection.hpp
+++ include/line_detection/line_detection.hpp
@@ -20,12 +20,17 @@ class LineDetector
   // Standard destructor.
   ~LineDetector();
   // Set a new original image.
-  // Define all parameters: ROI-Cropping corners, two default lines (original coordinates), 
+  setImage(Mat new_image);
+  // Define all parameters: ROI-Cropping corners, two default lines (original coordinates),...
+  setParams(Point2f roi_left_top, Point2f roi_right_bottom, Vec2f default_left, Vec2f default_right);
   // Does the Hough-Transform and draws the lines.
   void houghTransform(Mat &contours, Mat &draw_to, vector<Vec2f> &lines_hT, int threshold);
   // Function which returns ALL lines in the original coordinate system.
+  vector<Vec2f> getAllLinesOrig();
   // Function which returns ALL lines in the cropped coordinate system.
+  vector<Vec2f> getAllLinesCrop();
   // Method which deletes the lines vectors.
+  void clearLines();
   
   // PUBLIC MEMBER VARIABLES.
 
@@ -52,12 +57,17 @@ class LineDetector
   // ???
   Mat FindGray(Mat src);
   // Method which transforms rho and theta from cropped image (ROI) to original image.
+  Vec2f polarParamCrop2Orig(Vec2f polar_param_crop);
   // Method which transforms rho and theta from original image to cropped image (ROI).
+  Vec2f polarParamOrig2Crop(Vec2f polar_param_orig);
   // Function which does all kind of line detections for a given image and returns the lines.
+  vector<Vec2f> getAllLines(Mat &color_image);
   // Draws lines to an image (both have to have the same coordinate system!) by getting a vector with (rho, theta).
+  void drawLines2Image(Mat &draw_to, vector<Vec2f> lines_to_draw);
 
   // PRIVATE MEMBER VARIABLES.
-  // Store parameters of two default lines (original coordinates) --> Needed for resetting.
+  // Store parameters of two default lines left/right (original coordinates) --> Needed for resetting.
+  vector<Vec2f> default_lines[2];
   // Store coordinates of cropped image (top left and bottom right corner) in original coordinates.
   vector<Point2f> cropping_corners[2];
   // Store original color image.
@@ -66,4 +76,8 @@ class LineDetector
   cv::Mat cropped_;
 
   // Store all of the detected lines in the original coordinate system.
+  vector<Vec2f> all_lines_orig;
+  // Store all of the detected lines in the cropped coordinate system.
+  vector<Vec2f> all_lines_cropped;
+
 };
