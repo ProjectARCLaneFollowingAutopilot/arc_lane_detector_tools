2c1a193774afb7147a2f8a8d709d90d8d3eb9faf
NikkukkiN <nikku_alatur@yahoo.de>
2017-06-13 13:08:27 +0200

    RANSAC can be merged safely!

--- include/line_detection/line_detection.hpp
+++ include/line_detection/line_detection.hpp
@@ -2,11 +2,14 @@
 /* DESCRIPTION:
 This class offers method to find lines in an image, using different image processing methods.
 However, all are based on the hough line transform, but the images are preprocessed differently.
+At the end, the algorithm should return only two lines.
 */
 #include <cv.h>
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
-#include <cv.h>
+#include <vector>
+
+using namespace cv;
 
 class LineDetector
 {
@@ -16,9 +19,13 @@ class LineDetector
   LineDetector();
   // Standard destructor.
   ~LineDetector();
+  // Set a new original image.
+  // Define all parameters: ROI-Cropping corners, two default lines (original coordinates), 
   // Does the Hough-Transform and draws the lines.
   void houghTransform(Mat &contours, Mat &draw_to, vector<Vec2f> &lines_hT, int threshold);
-
+  // Function which returns ALL lines in the original coordinate system.
+  // Function which returns ALL lines in the cropped coordinate system.
+  // Method which deletes the lines vectors.
   
   // PUBLIC MEMBER VARIABLES.
 
@@ -26,16 +33,37 @@ class LineDetector
   private:
   // PRIVATE MEMBER METHODS.
   // Line finding methods:
+  // ???
   vector<Vec2f> HoughClassic (Mat &src_HC);
+  // ???
   vector<Vec2f> InRange (Mat src_IR);
+  // ???
   vector<Vec2f> GrayProperty (Mat src_GP);
+  // ???
   vector<Vec2f> CompareGray (Mat src_CG);
 
   // Helper methods:
+  // ???
   Mat showChannel(Mat RGB, bool B, bool G, bool R);
+  // ???
   Mat RoadThreshold(Mat src_RT);
+  // ???
   Vec3b IntensityOfArea(Mat &src_IOA, int x_gray, int y_gray, int width_gray, int height_gray);
+  // ???
+  Mat FindGray(Mat src);
+  // Method which transforms rho and theta from cropped image (ROI) to original image.
+  // Method which transforms rho and theta from original image to cropped image (ROI).
+  // Function which does all kind of line detections for a given image and returns the lines.
+  // Draws lines to an image (both have to have the same coordinate system!) by getting a vector with (rho, theta).
 
   // PRIVATE MEMBER VARIABLES.
-  
+  // Store parameters of two default lines (original coordinates) --> Needed for resetting.
+  // Store coordinates of cropped image (top left and bottom right corner) in original coordinates.
+  vector<Point2f> cropping_corners[2];
+  // Store original color image.
+  cv::Mat original_; 
+  // Store cropped color image (from original).
+  cv::Mat cropped_;
+
+  // Store all of the detected lines in the original coordinate system.
 };
--- src/line_detection/line_detection.cpp
+++ src/line_detection/line_detection.cpp
@@ -4,7 +4,7 @@
  // Standard constructor.
  LineDetector::LineDetector()
  { 
- 	std::cout<<"Linedetector created!"
+ 	std::cout<<"Linedetector created!"<<std::endl;
  }
  // Standard destructor.
  LineDetector::~LineDetector()
@@ -14,25 +14,7 @@
  // Does the Hough-Transform and draws the lines.
  void LineDetector::houghTransform(Mat &contours, Mat &draw_to, vector<Vec2f> &lines_hT, int threshold)
  {
- 	//Hough transform.
- 	HoughLines(contours, lines_hT, 1, CV_PI/180, threshold, 0, 0);
-	// Iterate through all the lines and draw the line. 
-	for(int i = 0; i < lines_hT.size(); i++)
-	{
-		float rho = lines_hT[i][0];
-		float theta = lines_hT[i][1];
-		Point pt1;
-		Point pt2;
-		double a = cos(theta);
-		double b = sin(theta);
-		double x0 = a*rho;
-		double y0 = b*rho;
-		pt1.x = cvRound(x0 + 1000*(-b));
-		pt1.y = cvRound(y0 + 1000*(a));
-		pt2.x = cvRound(x0 - 1000*(-b));
-		pt2.y = cvRound(y0 - 1000*(a));
-		line(draw_to, pt1, pt2, Scalar(0, 255, 0), 3, CV_AA);
-	}
+
  }
 
  // PUBLIC MEMBER VARIABLES.
@@ -42,49 +24,35 @@
  // Line finding methods.
  vector<Vec2f> LineDetector::HoughClassic (Mat &src_HC)
  {
- 	// Filter the image.
-  	Mat src_HC_roi_filtered = src_HC.clone();
-  	medianBlur(src_HC, src_HC_roi_filtered, 15);
 
-  	Mat contours = src_HC_roi_filtered.clone();
-  	// Run Canny.
-  	Canny(src_HC_roi_filtered, contours, 30, 50);
-	Mat draw_detected_hough = src_HC.clone();
-  	vector<Vec2f> lines_HC;
-	// Do HoughTransform.
-  	houghTransform(contours, draw_detected_hough, lines_HC, 90);
-  	return lines_HC;
  }
  vector<Vec2f> LineDetector::InRange (Mat src_IR)
  {
-   	// Calculate a mask by using the openCV-function inRange.
-  	Mat mask(src_IR.rows, src_IR.cols, CV_8UC1);
-	inRange(src_IR, Scalar(40, 40, 40),Scalar(150, 150, 150), mask);
-	Mat gray(src_IR.rows, src_IR.cols, CV_8UC1);
-	Mat contours(src_IR.rows, src_IR.cols, CV_8UC1);
-	Mat conv(src_IR.rows, src_IR.cols, CV_8UC1, 255);
-	// Apply the mask to the source image. 
-	bitwise_and(src_IR, src_IR, conv, mask);
-	Canny(mask, contours, 120, 150);
-	vector<Vec2f> lines_IR;
-	houghTransform(contours, src_IR ,lines_IR, 80);
-	return lines_IR;
+
  }
  vector<Vec2f> LineDetector::GrayProperty (Mat src_GP)
  {
- 	vector<Vec2f> lines_GP;
-	Mat contours(src_GP.rows, src_GP.cols, CV_8UC1);
-	// Find gray areas using the function FindGray.
-	Mat gray = FindGray(src_GP);
-	Canny(gray, contours, 50, 150);
-	houghTransform(contours, src_GP, lines_GP, 40);
-	return lines_GP;
+
  }
- vector<Vec2f> LineDetector::CompareGray (Mat src_CG);
+ vector<Vec2f> LineDetector::CompareGray (Mat src_CG)
+ {
+
+ }
 
  // Helper methods:
- Mat LINEDETECTOR::showChannel(Mat RGB, bool B, bool G, bool R);
- Mat LINEDETECTOR::RoadThreshold(Mat src_RT);
- Vec3b LINEDETECTOR::IntensityOfArea(Mat &src_IOA, int x_gray, int y_gray, int width_gray, int height_gray);
+ Mat LineDetector::showChannel(Mat RGB, bool B, bool G, bool R)
+ {
 
- // PRIVATE MEMBER VARIABLES.
+ }
+ Mat LineDetector::RoadThreshold(Mat src_RT)
+ {
+
+ }
+ Vec3b LineDetector::IntensityOfArea(Mat &src_IOA, int x_gray, int y_gray, int width_gray, int height_gray)
+ {
+
+ }
+ Mat LineDetector::FindGray(Mat src)
+ {
+
+ }
